8082437e9dea5945359da52b8fa7bcb39d37ffe5
Will G <wgrout87@gmail.com>
2023-07-16 14:53:17 -0600

    drew player object to canvas

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello World!");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
+
+const player = new Player(100, 100, 30, 'blue');
+player.draw();
+
+console.log(player);
+
